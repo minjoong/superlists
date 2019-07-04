881f8d6259b842ffb3a1245b90a52238c915cbbf
Minjoong Kim <minjoong.slow@gmail.com>
2019-07-05 04:03:00 +0900

    POST 요청 처리를 위한 뷰, 템플릿, 테스트 추가

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,16 @@
+import re
+
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
 
 from lists.views import home_page
+
+
+def remove_csrf(html_code):
+    csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^<]+>'
+    return re.sub(csrf_regex, '', html_code)
 
 
 class HomePageTest(TestCase):
@@ -14,6 +21,22 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
+
         response = home_page(request)
+
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(remove_csrf(response.content.decode()), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': '신규 작업 아이템'}
+        )
+        self.assertEqual(remove_csrf(response.content.decode()), expected_html)
